970b111cad66372f7185ab8f075a58e3ac9b6903
leevi777 <leevi777@163.com>
2018-06-22 16:40:14 +0800

    加入显示天气信息功能

--- app/src/main/java/com/example/test/coolweather/util/Utility.java
+++ app/src/main/java/com/example/test/coolweather/util/Utility.java
@@ -5,15 +5,16 @@ import android.text.TextUtils;
 import com.example.test.coolweather.db.City;
 import com.example.test.coolweather.db.County;
 import com.example.test.coolweather.db.Province;
+import com.example.test.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * 解析和处理服务器返回的省级数据
- */
-
+    /**
+     * 解析和处理服务器返回的省级数据
+     */
 public class Utility {
     public static boolean handleProvinceResponse(String response){
         if (!TextUtils.isEmpty(response)){
@@ -34,10 +35,9 @@ public class Utility {
         return false;
     }
 
-/**
- * 解析和处理服务器返回的市级数据
- */
-
+    /**
+     * 解析和处理服务器返回的市级数据
+    */
     public static boolean handleCityResponse(String response,int provinceId){
         if (!TextUtils.isEmpty(response)){
             try {
@@ -58,10 +58,9 @@ public class Utility {
         return false;
     }
 
-/**
- * 解析和处理服务器返回县级数据
- */
-
+    /**
+     * 解析和处理服务器返回县级数据
+    */
     public static boolean handleCountyResponse(String response,int CityId){
         if (!TextUtils.isEmpty(response)){
             try {
@@ -80,6 +79,21 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+    * 将返回的JSON数据解析成Weather实体类
+    */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
